1d011f4bf9ae977029c345c2f2da68a5aecc1859
jabelic <hibiki990523@gmail.com>
2023-02-05 04:33:15 +0900

    some

--- src/generator.ts
+++ src/generator.ts
@@ -30,4 +30,24 @@ export const generator=()=>{
     console.log( g2.next(5) ); // { value: 8, done: false }
     // yield "second" の部分が 5 に置き換えられる : b = 5
     console.log( g2.next() ); // { value: undefined, done: true }
+
+
+    function* gfn3(){
+        yield* [1, 3, 5];
+    }
+    var g3 = gfn3();
+
+    console.log( g3.next() ); // { value: 1, done: false }
+    console.log( g3.next() ); // { value: 3, done: false }
+    console.log( g3.next() ); // { value: 5, done: false }
+    console.log( g3.next() ); // { value: undefined, done: true }
+
+    /** ジェネレータを使えば、処理を止めることができるため、以下のようにもできる */
+    // function* gfn4(){
+    //     alert("こんにちは！"); yield;
+    //     alert("良い天気ですね。"); yield;
+    //     alert("さようなら！");
+    // }
+    // var g4 = gfn4();
+    // document.onclick = function(){ g4.next(); }; // ページをクリックするたびに g.next(); を実行する
 }
